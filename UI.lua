38b5908e9c4225be7fe0d8ffa6736e7a0ec02a7c
xKimjikungX <102714267+xKimjikungX@users.noreply.github.com>
2022-04-02 10:01:07 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -92,7 +92,7 @@ function create:Win()
     Main.Parent = Atomlib
     Main.BackgroundColor3 = Color3.fromRGB(19 , 19, 19)
     Main.BorderSizePixel = 0
-    Main.Position = UDim2.new(0.5, 0, 0.5   , 0)
+    Main.Position = UDim2.newUDim2.new(0,25,0,15)
     Main.Size = UDim2.new(0, 0, 0, 0)
     Main.ClipsDescendants = true 
     Main.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -103,7 +103,7 @@ function create:Win()
     Logo.Name = "Logo"
     Logo.Parent = Main
     Logo.AnchorPoint = Vector2.new(0.5, 0.5)
-    Logo.Position = UDim2.new(0, 0, 0.0, 0)
+    Logo.Position = UDim2.new(0.1, 0, 0.08, 0)
     Logo.BackgroundColor3 = Color3.fromRGB(19, 19, 19)
     Logo.BorderColor3 = Color3.fromRGB(27, 42, 53)
     Logo.BorderSizePixel = 0
