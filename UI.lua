e1fd4ba5bc9485b8781e121ed6fd56344c932aa7
xKimjikungX <102714267+xKimjikungX@users.noreply.github.com>
2022-04-02 09:26:03 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -107,7 +107,7 @@ function create:Win()
     Logo.BackgroundColor3 = Color3.fromRGB(19, 19, 19)
     Logo.BorderColor3 = Color3.fromRGB(27, 42, 53)
     Logo.BorderSizePixel = 0
-    Logo.Size = UDim2.new(0, 56, 0, 56)
+    Logo.Size = UDim2.new(0, 42, 0, 42)
     Logo.Image = "http://www.roblox.com/asset/?id=9010576467"
     
     local PageTap = Instance.new("Frame")
